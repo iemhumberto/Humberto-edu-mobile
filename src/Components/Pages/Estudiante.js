43df98340668bdb95560a715981a1c34701ba8ad
CSamboni <elementrixz512@gmail.com>
2018-10-12 05:54:31 -0500

    Trabajando en seccion de estudiante

--- src/Components/Pages/Estudiante.js
+++ src/Components/Pages/Estudiante.js
@@ -1,11 +1,113 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import '../../App.css';
 
+/*Import de Data tools*/
+import data from '../Requests/ToolsEst';
 
 class Estudiante extends Component {
+    tools() {
+        return data
+            .tools
+            .map(tool => {
+                return (
+                    <div className="column is-mobile" key={tool.id}>
+                        <div
+                            style={{
+                            'borderRadius': '5px'
+                        }}className="card">
+                            <div
+                                style={{
+                                'margin': '2px'
+                            }}
+                                className="card-header">
+                                <strong>{tool.title}</strong>
+                            </div>
+                            <img
+                                style={{
+                                'backgroundPosition': 'center'
+                            }}
+                                src={tool.ImageUrl}
+                                alt="Imagen de tool"/>
+                            <div
+                                style={{
+                                'display': 'flex',
+                                'backgroundColor': `${tool.color}`,
+                                'margin': 'auto',
+                                'width': '30%'
+                            }}
+                                className="tag is-info">{tool.Category}</div>
+                            <div
+                                style={{
+                                'textAlign': 'center'
+                            }}
+                                className="content">{tool.description}</div>
+                            <a href={tool.link}>
+                                <div
+                                    style={{
+                                    'display': 'flex',
+                                    'margin': 'auto',
+                                    'justifyContent': 'center'
+                                }}
+                                    className="button is-primary is-outlined">Usar herramienta</div>
+                            </a>
+                        </div>
+                    </div>
+                )
+            })
+    }
+
     render() {
-        return(
+        return (
             <section className="column is-mobile">
-            <h2>Este es el component de estudiante.</h2>
+                <div className="content has-text-centered">
+                    <div
+                        style={{
+                        'display': 'flex',
+                        'justifyContent': 'center',
+                        'borderRadius': '5px',
+                        'color': 'white',
+                        'backgroundColor': 'black',
+                        'alignItems': 'center',
+                        'width': '100%',
+                        'height': '60px'
+                    }}
+                        className="title agenda-informativa-title">Agenda informativa</div>
+                </div>
+                <div className="message is-info">
+                    <div className="message-body">
+                        <p>Elija su jornada académica para ver más.</p>
+                    </div>
+                </div>
+                <div
+                    style={{
+                    'display': 'flex',
+                    'justifyContent': 'center',
+                    'alignSelf': 'center'
+                }}>
+                    <Link to="/agenda-mañana">
+                        <div
+                            style={{
+                            'margin': 'auto',
+                            'marginRight': '5px'
+                        }}
+                            className="button is-primary">
+                            <p>Jornada Mañana</p>
+                        </div>
+                    </Link>
+                    <Link to="/agenda-tarde">
+                        <div
+                            style={{
+                            'margin': 'auto'
+                        }}
+                            className="button is-info">
+                            <p>Jornada Tarde</p>
+                        </div>
+                    </Link>
+                </div>
+                <div>
+                    {this.tools()}
+                </div>
             </section>
         );
     }
---
+++ src/Components/Requests/ToolsEst.js
@@ -0,0 +1,57 @@
+export default {
+    tools : [
+        {
+            id: 1,
+            title: 'Nuclino',
+            Category: 'Presentaciones',
+            ImageUrl: 'https://d33wubrfki0l68.cloudfront.net/0d5f11ddd1bebaa00cac1831eb892e623c3a39bb/2' +
+                    '090d/img/press/nuclino_logo_preview.png',
+            description: 'Herramienta para presentaciones wiki colaborativas.',
+            link: 'https://www.nuclino.com/',
+            color: '#00d1b2'
+        }, {
+            id: 2,
+            title: 'Canva',
+            Category: 'Presentaciones',
+            ImageUrl: 'http://www.bestmediainfo.com/wp-content/uploads/2015/10/canva.jpg',
+            description: 'Diseño de presentaciones. Interesantes y atractivas.',
+            link: 'https://www.canva.com/es_ar/crear/presentaciones/',
+            color: '#00d1b2'
+        }, {
+            id: 3,
+            title: 'Microsoft To-Do',
+            ImageUrl: 'https://mk0intowindows84fvao.kinstacdn.com/wp-content/uploads/2017/04/delete-Mic' +
+                    'rosfot-To-Do-account_thumb.png',
+            Category: 'Productividad',
+            description: 'Diseño de presentaciones grupales y a distancia.',
+            link: 'https://todo.microsoft.com/es-es',
+            color: '#ffdd57'
+        }, {
+            id: 4,
+            title: 'Trello',
+            ImageUrl: 'https://png.icons8.com/color/1600/trello.png',
+            Category: 'Productividad',
+            description: 'Organize su calendario. Tareas. Haga todo estructurado. Fácil.',
+            link: 'https://trello.com/',
+            color: '#ffdd57'
+        }, {
+            id: 5,
+            title: 'Evernote',
+            Category: 'Escritura',
+            ImageUrl: 'https://s24953.pcdn.co/blog/wp-content/uploads/2018/01/evernote-logo-wide.png',
+            description: 'Evernote te ayuda a capturar y establecer la prioridad de las ideas, proyectos y' +
+                    ' listas de tareas.',
+            link: 'https://evernote.com/intl/es/',
+            color: '#209cee'
+        }, {
+            id: 6,
+            title: 'Medium',
+            ImageUrl: 'https://media.wired.com/photos/5a55a95af41e4c2cd9ee6cd8/master/pass/Medium-logo-' +
+                    'canvas.jpg',
+            Category: 'Escritura',
+            description: 'Haga de sus escritos, historias.',
+            link: 'https://medium.com/',
+            color: '#209cee'
+        }
+    ]
+}
